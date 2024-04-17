bad03969aae358c428e187291ae9922d4b6d312c
Riham <rihammohareb6@gmail.com>
2024-04-17 16:59:59 +0200

    Generic Repositry

---
+++ Talabat.Core/Repositries.Contract/IGenericRepositry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Core.Repositries.Contract
+{
+    public interface IGenericRepositry<T>where T:BaseEntity 
+    {
+        Task<T?> GetAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
---
+++ Talabat.Repositry/GenericRepositry.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositries.Contract;
+using Talabat.Repositry.Data;
+
+namespace Talabat.Repositry
+{
+    public class GenericRepositry<T> : IGenericRepositry<T> where T : BaseEntity
+    {
+        private readonly StoreContext _dbContext;
+
+        public GenericRepositry(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+
+        }
+
+        public async Task<T?> GetAsync(int id)
+        {
+            return await _dbContext .Set<T>().FindAsync(id);
+        }
+    }
+}
