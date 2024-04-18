965d072744a7adf5ee9c1512a68724f828a06d6c
Riham <rihammohareb6@gmail.com>
2024-04-18 03:17:06 +0200

    NotFound,BadRequest,Unauthrized Error Handling

---
+++ Talabat.Route.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Talabat.Repositry.Data;
+using Talabat.Route.APIs.Errors;
+
+namespace Talabat.Route.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : BaseAPIController 
+    {
+        private readonly StoreContext _dbcontext;
+
+        public BuggyController(StoreContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        [HttpGet("NotFound")]
+        public IActionResult GetNotFoundRequest()
+        {
+            var product = _dbcontext.Products.Find(100);
+            if (product == null)
+            {
+                return NotFound(new APIResponse(404));
+            }
+            return Ok(product);
+        }
+        [HttpGet("ServerError")]
+        public IActionResult GetServerErrorRequest()
+        {
+            var product = _dbcontext.Products.Find(100);
+
+            var prodectToReurn = product.ToString();
+
+            return Ok(prodectToReurn);
+        }
+        [HttpGet("BadRequest")]
+        public IActionResult GetbadRequestRequest()
+        {
+
+
+            return BadRequest(new APIResponse(400));
+        }
+        [HttpGet("BadRequest/{id}")]//Validation Error
+        public IActionResult GetbadRequestRequest(int id)
+        {
+
+
+            return Ok();
+        }
+        [HttpGet("UnAuthorixedError")]
+        public IActionResult GetUnAuthorixedErrorRequest()
+        {
+
+
+            return Unauthorized(new APIResponse(401));
+        }
+    }
+}
---
+++ Talabat.Route.APIs/Errors/APIResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Talabat.Route.APIs.Errors
+{
+    public class APIResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public APIResponse(int statuscode, string? message = null)
+        {
+            StatusCode = statuscode;
+
+            Message = message ?? GetDefaultMessageForStatusCode(statuscode);
+
+        }
+
+        private string? GetDefaultMessageForStatusCode(int statuscode)
+        {
+            return statuscode switch
+            {
+                400 => "A Bad Request You Have Made",
+                401 => "Authorized you are not",
+                404 => "Resource was not Found",
+                500 => "Error Are the Path For The Dark Side,Errors Lead To Anger Anger Lead to ,Hate Lead to Carrer Change"
+            };
+        }
+    }
+}
