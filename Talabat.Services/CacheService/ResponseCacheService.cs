b14c06c14c660acae35bd2fec93883eae01274b1
Riham <rihammohareb6@gmail.com>
2024-05-30 23:49:54 +0300

    Cashing Services

---
+++ Talabat.Services/CacheService/ResponseCacheService.cs
@@ -0,0 +1,39 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Services.Contract;
+
+namespace Talabat.Services.CacheService
+{
+	public class ResponseCacheService : IResponseCacheServic
+	{
+		private readonly IDatabase _database;
+		public ResponseCacheService(IConnectionMultiplexer redis)
+		{
+			_database = redis.GetDatabase();
+
+		}
+		public Task CacheResponseAsync(string Key, object Response, TimeSpan timeToLive)
+		{
+			if (Response is null) return ;
+
+			var serializeOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+			var serializedResponse = JsonSerializer.Serialize(Response, serializeOptions);
+
+			await _database.StringSetAsync(Key, serializedResponse, timeToLive);
+		}
+
+		public Task<string?> GetCachedResponseAsync(string Key)
+		{
+			var response = await _database.StringGetAsync(Key);
+
+			if (response.IsNullOrEmpty) return null;
+
+			return response;
+		}
+	}
+}
